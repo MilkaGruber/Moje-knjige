4f00a835586b025451774b5160d73c2c7b8ab679
MilkaGruber <gruber.milka@gmail.com>
2021-10-18 23:04:14 +0200

    popravljene metode v razredu Knjige

--- model.py
+++ model.py
@@ -2,11 +2,12 @@ from datetime import date
 
 class Model:
     def __init__(self):
-        self.razdelek = Razdelek(['Želim prebrati', 'Berem', 'Že prebrano'])
+        self.razdelek = ['Želim prebrati', 'Berem', 'Že prebrano']
         self.aktualni_razdelek = None
 
     def zamenjaj_razdelek(self, razdelek):
         self.aktualni_razdelek = razdelek
+
 
 class Razdelek:
     def __init__(self, ime):
@@ -16,8 +17,12 @@ class Razdelek:
     def dodaj_knjige(self, knjiga):
         self.knjige.append(knjiga)
 
+    def stevilo_knjig_v_razdelku(self):
+        return len(self.knjige)
+
+
 class Knjige:
-    def __init__(self, naslov, avtor, st_strani):
+    def __init__(self, naslov, avtor, st_strani, faza_branja=False):
         self.naslov = naslov
         self.avtor = avtor
         self.st_strani = st_strani
@@ -27,12 +32,13 @@ class Knjige:
     def zacni_brati(self):
         self.faza_branja = None
 
+    def preberi_n_strani(self, n):
+        self.st_prebranih_strani += n
+
     def preberi(self):
         self.st_prebranih_strani = self.st_strani
         self.faza_branja = True
 
-    def preberi_n_strani(self, n):
-        self.st_prebranih_strani += n
         
     
 
