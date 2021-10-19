d6b6c05aad62e3bb2aef478481538fa73fb06ef0
MilkaGruber <gruber.milka@gmail.com>
2021-10-19 18:51:29 +0200

    V grobem končan model

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import datetime
 
 class Model:
     def __init__(self):
@@ -10,11 +10,14 @@ class Model:
 
 class Razdelek:
     def __init__(self, ime):
-        prvi_razdelek = 'Želim prebrati knjige'
-        drugi_razdelek = 'Trenutno berem knjige'
-        tretji_razdelek = 'Seznam že prebranih knjig'
+        prvi_razdelek = 'ZELIM PREBRATI'
+        drugi_razdelek = 'V BRANJU' 
+        tretji_razdelek = 'PREBRANA'
         self.ime = ime
         self.knjige = []
+
+    def __str__(self):
+        return self.ime + ': ' + str(self.knjige)
 
     def dodaj_knjige(self, knjiga):
         self.knjige.append(knjiga)
@@ -24,32 +27,51 @@ class Razdelek:
 
 
 class Knjige:
-    def __init__(self, naslov, avtor, st_strani, faza_branja=False):
+    def __init__(self, naslov, avtor, st_strani, faza_branja='ZELIM PREBRATI'):
         self.naslov = naslov
         self.avtor = avtor
         self.st_strani = st_strani
-        self.faza_branja = False
+        self.faza_branja = faza_branja
         self.st_prebranih_strani = 0
-    
-    def zacni_brati(self, datum=None):
-        self.faza_branja = None #mogoče to funkcijo zbriši
-        self.datum = date.today()
+        self.datum = datetime.now()
+        self.zgodovina = [(0, self.datum)] #seznam bo opisoval, kdaj sem prebrala koliko strani
 
-    def preberi_n_strani(self, n, datum=None):
+    def __str__(self):
+        return f"{self.faza_branja}: {self.naslov}, {self.avtor}, prebranih {self.st_prebranih_strani} strani od {self.st_strani}"
+    
+    def zacni_brati(self):
+        self.faza_branja = 'V BRANJU' 
+        self.datum = datetime.now()
+        self.zgodovina.append((0, self.datum))
+
+    def preberi_n_strani(self, n):
         self.st_prebranih_strani += n
-        self.datum = date.today()
-        if self.st_prebranih_strani == self.st_strani:
+        self.datum = datetime.now()
+        if  self.faza_branja == 'ZELIM PREBRATI': 
+            self.zacni_brati()
+            self.zgodovina.append((self.st_prebranih_strani, self.datum))
+        elif self.st_prebranih_strani == self.st_strani:
             self.preberi()
+        else:
+            self.faza_branja = 'V BRANJU'
 
-    def preberi(self, datum=None):
+    def preberi(self):
+        if self.faza_branja == 'ZELIM PREBRATI':
+            self.zacni_brati
         self.st_prebranih_strani = self.st_strani
-        self.faza_branja = True
-        self.datum = date.today()
+        self.faza_branja = 'PREBRANA'
+        self.datum = datetime.now()
+        self.zgodovina.append((self.st_prebranih_strani, self.datum))
 
+    def delez_prebrane_knjige(self):
+        st_prebranih = self.st_prebranih_strani
+        st_strani = self.st_strani
+        return int((st_prebranih * 100) // st_strani)
 
-        
-    
-
-
-
+    def kako_dolgo_berem(self): # tudi ne dela
+        if self.faza_branja != 'ZELIM PREBRATI':
+            zacetek = self.zgodovina[1][1]
+            konec = self.zgodovina[-1][1]
+            print((zacetek, konec))
+            return konec - zacetek
 
