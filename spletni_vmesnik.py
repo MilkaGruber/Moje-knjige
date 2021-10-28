f98f76274ee365b1c78c1a20365d422eac71c37c
MilkaGruber <gruber.milka@gmail.com>
2021-10-28 18:07:35 +0200

    Združenih več strani v eno- osnovno

--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -14,38 +14,21 @@ RAZDELEK4 = model.razdelki[3]  # RAZDELEK Izhod
 
 @bottle.get('/')
 def osnovna_stran():
-    return bottle.template('osnovna_stran.tpl')
-
-@bottle.get('/seznam_zelja')
-def seznam_zelja():
     return bottle.template(
-        'seznam_zelja.tpl',
+        'osnovna_stran.tpl',
         st_knjig_sez_zelja=RAZDELEK1.stevilo_knjig_v_razdelku(),
-        knjige_sez_zelja=RAZDELEK1.knjige
-    )
-
-@bottle.get('/v_branju')
-def v_branju():
-    return bottle.template(
-        'v_branju.tpl',
+        knjige_sez_zelja=RAZDELEK1.knjige,
         st_knjig_v_branju=RAZDELEK2.stevilo_knjig_v_razdelku(),
-        knjige_v_branju=RAZDELEK2.knjige
-    )
-
-@bottle.get('/prebrano')
-def prebrano():
-    return bottle.template(
-        'prebrano.tpl',
+        knjige_v_branju=RAZDELEK2.knjige,
         st_knjig_prebrano=RAZDELEK3.stevilo_knjig_v_razdelku(),
         prebrane_knjige=RAZDELEK3.knjige
     )
 
-@bottle.get('/dodaj/')
+@bottle.post('/dodaj/')
 def dodaj_knjigo():
-
-    naslov = bottle.request.query['naslov']
-    avtor = bottle.request.query['avtor']
-    st_strani = bottle.request.query['st_strani']
+    naslov = bottle.request.forms.getunicode("naslov")
+    avtor = bottle.request.forms.getunicode("avtor")
+    st_strani = bottle.request.forms.getunicode("st_strani")
     knjiga = Knjige(naslov, avtor, st_strani)
     RAZDELEK1.dodaj_knjigo(knjiga)
     model.shrani_v_datoteko(IME_DATOTEKE)
